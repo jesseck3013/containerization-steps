18056eec071603d2b3c1b485ec9b571b34c6d485
jesseck3013 <jesseck3013@outlook.com>
2025-04-29 17:28:43 +0800

    added postgres connector

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/jackc/pgx/v5"
 	"io"
 	"log"
 	"net/http"
@@ -24,6 +26,40 @@ func readFile(path string) string {
 	return string(b)
 }
 
+func toHtmlList(strs []string) string {
+	var res string = "<ul>"
+	for _, str := range strs {
+		res += fmt.Sprintf("<li>%s</li>", str)
+	}
+	res += "</ul>"
+	return res
+}
+
+func queryPg() string {
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	rows, err := conn.Query(context.Background(), "select name from test")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	users := []string{}
+	for rows.Next() {
+		var name string
+		rows.Scan(&name)
+		users = append(users, name)
+	}
+
+	return toHtmlList(users)
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>This is an example web app written in Go.</h1>")
@@ -32,6 +68,12 @@ func main() {
 	http.HandleFunc("/text", func(w http.ResponseWriter, r *http.Request) {
 		data := readFile("./text")
 		fmt.Fprint(w, data)
+	})
+
+	http.HandleFunc("/pg", func(w http.ResponseWriter, r *http.Request) {
+		data := queryPg()
+
+		fmt.Fprint(w, fmt.Sprintf("<h1>Users in Postgres</h1>%s", data))
 	})
 
 	log.Println("The web app starts listening at port :8080")
